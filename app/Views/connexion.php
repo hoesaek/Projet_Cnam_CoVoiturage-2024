80e3cd4ee6c0a0be672b0dd27d243f4acd0545ba
ML <lolopechka9@gmail.com>
2024-05-28 11:45:22 +0200

    redirect issues

--- app/Views/connexion.php
+++ app/Views/connexion.php
@@ -31,7 +31,7 @@ if (isset($_GET['error'])) {
 ?>
 
 <div class="form-container">
-  <form action="/Account/login-process.php" method="POST" class="max-w-sm mx-auto login" id="loginForm">
+  <form action="<?php require_once __DIR__ . "/Account/login-process.php" ;?>" method="POST" class="max-w-sm mx-auto login" id="loginForm">
     <div class="mb-5">
       <label for="email_login" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre e-mail</label>
       <input type="email" id="email_login" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="exemple.auditeur@lecnam.net" required />
@@ -43,7 +43,7 @@ if (isset($_GET['error'])) {
     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Soumettre</button>
   </form>
 
-  <form action="/Account/register.php" method="POST" class="max-w-sm mx-auto register" id="registerForm">
+  <form action="<?php require_once __DIR__ . "/Account/register-process.php";?>" method="POST" class="max-w-sm mx-auto register" id="registerForm">
     <div class="mb-5">
       <label for="email_register" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
       <input type="email" id="email_register" name="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="exemple.auditeur@lecnam.net" required />
